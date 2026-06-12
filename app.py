e328315f01eff9584f4ec98f95de1b4cc8789609
hungthichlamgame <phongkd.chemco@gmail.com>
2026-06-12 21:58:23 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_socketio import SocketIO, join_room, leave_room, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
 DATABASE = 'game.db'
 
